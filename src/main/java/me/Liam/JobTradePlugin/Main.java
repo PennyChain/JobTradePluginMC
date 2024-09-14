bdf79dca13117159ec56a046768426ecf81e5f82
Ribe <afs0016376@agderskolen.no>
2024-09-14 19:11:28 +0200

    Second commit

--- src/main/java/me/Liam/JobTradePlugin/Main.java
+++ src/main/java/me/Liam/JobTradePlugin/Main.java
@@ -1,4 +1,4 @@
-package me.LiamRibe.JobTradePlugin;
+package me.Liam.JobTradePlugin;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
